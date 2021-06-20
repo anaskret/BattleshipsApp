3144143e15890567db9c8c36bb23e490f29a8974
Monika Nogała <mxnogala@gmail.com>
2021-06-20 11:26:28 +0200

    player endpoint fixed

--- Battleships.Server/Battleships.Repositories/Repositories/PlayerRepository.cs
+++ Battleships.Server/Battleships.Repositories/Repositories/PlayerRepository.cs
@@ -4,7 +4,7 @@ using Battleships.Repositories.Repositories.Generic;
 using Battleships.Repositories.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
